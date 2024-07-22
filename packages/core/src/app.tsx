eee069e039661dce8c49e087a422a6de5c542ac3
dinwwwh <dinwwwh@gmail.com>
2024-07-22 20:36:07 +0700

    feat: $btn

--- packages/core/src/app.tsx
+++ packages/core/src/app.tsx
@@ -20,6 +20,30 @@ export function App() {
         </button>
       </div>
 
+      <div className={classes.component}>
+        <input className="$input" placeholder="Enter your first name" />
+        <input className="$input" placeholder="Enter your last name" disabled />
+      </div>
+
+      <div className={classes.component}>
+        <input className="$input $input-sm" placeholder="Enter your first name" />
+        <input className="$input $input-sm" placeholder="Enter your last name" value="with value" disabled />
+      </div>
+
+      <div className={classes.component} style={{ maxWidth: 360 }}>
+        <input className="$input" placeholder="Enter your first name" />
+        <button className="$btn">Button</button>
+      </div>
+
+      <div className={classes.component} style={{ maxWidth: 200 }}>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <input className="$input $input-sm" placeholder="Enter your first name" style={{ marginRight: 8 }} />
+          <button className="$btn $btn-square $btn-sm">
+            <IconEdit />
+          </button>
+        </div>
+      </div>
+
     </div>
   )
 }

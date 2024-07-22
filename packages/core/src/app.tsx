650e1958bf141890ea1de6fee16c61ff7182036d
dinwwwh <dinwwwh@gmail.com>
2024-07-22 19:28:35 +0700

    feat: $btn-square

--- packages/core/src/app.tsx
+++ packages/core/src/app.tsx
@@ -1,3 +1,4 @@
+import { IconEdit } from '@tabler/icons-react'
 import classes from './app.module.css'
 
 export function App() {
@@ -6,7 +7,17 @@ export function App() {
 
       <div className={classes.component}>
         <button className="$btn">Button</button>
+        <button className="$btn $btn-square">
+          <IconEdit />
+        </button>
         <button className="$btn $btn-sm">Button</button>
+        <button className="$btn $btn-square $btn-sm">
+          <IconEdit />
+        </button>
+        <button className="$btn $btn-sm" disabled>Button</button>
+        <button className="$btn $btn-square $btn-sm" disabled>
+          <IconEdit />
+        </button>
       </div>
 
     </div>
